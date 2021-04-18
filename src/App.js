9826ad130fceef308fba45741bafbb1a558f321e
Dimas Angkasa Nurindra <dimas_angkasa_27rpl@student.smktelkom-mlg.sch.id>
2021-04-18 21:38:41 +0700

    Add Navbar and dashboard layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "./comopents/dashboard/Dashboard";
+import Navbar from "./comopents/layout/Navbar";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" component={Dashboard} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
